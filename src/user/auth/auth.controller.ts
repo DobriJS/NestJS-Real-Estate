cf13cd84a271e61cae67586205fbe879ee60e180
Dobri Dobrevski <53184157+DobriJS@users.noreply.github.com>
2022-10-04 16:30:07 +0300

    Complete Signin user logic

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { SignupDto } from '../dtos/auth.dto';
+import { SigninDto, SignupDto } from '../dtos/auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
+
     @Post('/signup')
     signup(@Body() body: SignupDto) {
         return this.authService.signup(body);
+    }
+
+    @Post('/signin')
+    signin(@Body() body: SigninDto) {
+        return this.authService.signin(body);
     }
 }
