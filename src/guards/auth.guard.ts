d901e3d4945851b971c6891a3e90eeb041dc77ba
Dobri Dobrevski <53184157+DobriJS@users.noreply.github.com>
2022-10-16 18:12:50 +0300

    Verify JWT token

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,17 +1,30 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private readonly reflector: Reflector) { }
 
-    canActivate(context: ExecutionContext) {
+    async canActivate(context: ExecutionContext) {
         const roles = this.reflector.getAllAndOverride<string[]>('roles', [
             context.getHandler(),
             context.getClass()
-        ])
+        ]);
 
-        console.log(roles);
+        if (roles.length) {
+            const request = context.switchToHttp().getRequest();
+            const token = request.headers?.authorization?.split('Bearer ')[1];
+            try {
+                const user = jwt.verify(token, process.env.JWT_KEY);
+                console.log({ user });
+                return true;
+
+            } catch (error) {
+                return false;
+            }
+        }
+
         return true;
     }
 }
